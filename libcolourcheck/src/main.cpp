75bb075b054557c1192d380c5c8abdf89d42e729
Marcos Reis <marcos.reis@reason.com.br>
2016-10-23 12:50:17 -0200

    Including test files and minor improvements

--- libcolourcheck/src/main.cpp
+++ libcolourcheck/src/main.cpp
@@ -28,14 +28,15 @@ using namespace std;
 using namespace colourcheck;
 namespace po = boost::program_options;
 
-int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, int timestamp);
+int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, int complete);
 
 int main(int argc, char **argv)
 {
     try {
         po::options_description generic("Generic");
         generic.add_options()
-        ("help,h", "This help message");
+        ("help,h", "This help message")
+        ("license,l", "License and copyright");
 
         po::options_description config("Configurations");
         config.add_options()
@@ -43,7 +44,7 @@ int main(int argc, char **argv)
         ("intensity,i", po::value<string>(), "Intensities input file")
         ("ciexyz,c", po::value<string>(), "Colour matching functions input file")
         ("output,o", po::value<string>()->default_value("output.csv"), "Output file")
-        ("timestamp,t", po::value<int>()->default_value(0), "Output each timestamp in file");
+        ("complete,t", po::value<int>()->default_value(0), "Output each timestamp in file");
 
         po::options_description desc("Allowed options");
         desc.add(generic).add(config);
@@ -52,10 +53,34 @@ int main(int argc, char **argv)
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
 
-        if (vm.count("help") || !vm.count("wavelength") || !vm.count("intensity") || !vm.count("ciexyz")) {
-            cout << desc << "\n";
+        if (vm.count("version")) {
+            cout <<
+                 "Copyright (C) 2016  Marcos Goulart Reis. All rights reserved." << endl << endl <<
+                 "This program is free software: you can redistribute it and/or modify" << endl <<
+                 "it under the terms of the GNU General Public License as published by" << endl <<
+                 "the Free Software Foundation, either version 3 of the License, or" << endl <<
+                 "(at your option) any later version." << endl << endl <<
+                 "This program is distributed in the hope that it will be useful," << endl <<
+                 "but WITHOUT ANY WARRANTY; without even the implied warranty of" << endl <<
+                 "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" << endl <<
+                 "GNU General Public License for more details." << endl << endl <<
+                 "You should have received a copy of the GNU General Public License" << endl <<
+                 "along with this program.  If not, see <http://www.gnu.org/licenses/>." << endl;
+
             return 0;
         }
+
+        if (vm.count("help") || !vm.count("wavelength") || !vm.count("intensity") || !vm.count("ciexyz")) {
+            cout << "This program was designed to solve the C++ Challenge from Thalmic Labs (VanHackathon)." << endl;
+            cout << desc << endl;
+            return 0;
+        }
+
+        cout << "Wavelength file: " << vm["wavelength"].as<string>() << endl;
+        cout << "Intensity file: " << vm["intensity"].as<string>() << endl;
+        cout << "Colour matching functions file: " << vm["ciexyz"].as<string>() << endl;
+        cout << "Output file: " << vm["output"].as<string>() << endl;
+        cout << "Complete output: " << vm["complete"].as<int>() << endl;
 
         Spectrometer s(vm["wavelength"].as<string>(), vm["intensity"].as<string>());
         ColourFunction cf(vm["ciexyz"].as<string>());
@@ -72,7 +97,7 @@ int main(int argc, char **argv)
             return 1;
         }
 
-        if (calculate_data(cf, s, vm["output"].as<string>(), vm["timestamp"].as<int>()) != 0) {
+        if (calculate_data(cf, s, vm["output"].as<string>(), vm["complete"].as<int>()) != 0) {
             return 1;
         }
     }
@@ -125,7 +150,7 @@ private:
     bool _comma;
 };
 
-int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, int timestamp)
+int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, int complete)
 {
     AverageFilter<double> x_filter;
     AverageFilter<double> y_filter;
@@ -136,13 +161,14 @@ int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, in
         return 1;
     }
 
-    if (timestamp) {
+    if (complete) {
         out.create_header("Timestamp,x,y");
     }
 
-    int temp_aux = 1;
+    int timestamp_proc = 0;
     for (auto it = s.begin(); it != s.end(); ++it) {
         vector<tuple<double, double>> data;
+        cout << "Processing timestamp #" << ++timestamp_proc << endl;
 
         size_t index = 0;
         for (auto intensity : *it) {
@@ -156,8 +182,8 @@ int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, in
         x_filter << get<0>(xyz);
         y_filter << get<1>(xyz);
 
-        if (timestamp) {
-            out.new_cell(temp_aux++);
+        if (complete) {
+            out.new_cell(timestamp_proc);
             out.new_cell(get<0>(xyz));
             out.new_cell(get<1>(xyz));
             out.new_line();
@@ -171,7 +197,7 @@ int calculate_data(ColourFunction &cf, Spectrometer &s, const string &output, in
     out.new_cell(x_filter.get_value());
     out.new_cell(y_filter.get_value());
     out.new_cell(uv_filter.get_standard_deviation());
-    
+
     out.new_line();
 
     return 0;
